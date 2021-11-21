26ced8606f192e03933a9dab25103418926577e0
jmjxperia <jmjxperia@users.noreply.github.com>
2021-11-21 14:00:22 +0530

    update verify submenu testcases

---
+++ src/main/java/pageObject/DashboardPage.java
@@ -0,0 +1,44 @@
+package pageObject;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashboardPage {
+
+    public DashboardPage(AppiumDriver<MobileElement> driver) {
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='Battery']")
+    public MobileElement battery;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='Network']")
+    public MobileElement network;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='RAM']")
+    public MobileElement ram;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='Storage']")
+    public MobileElement storage;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='CPU Status']")
+    public MobileElement cpuStats;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='Temperatures']")
+    public MobileElement temperatures;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='Monitors']")
+    public MobileElement monitors;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='Benchmarks']")
+    public MobileElement benchmarks;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='Tools']")
+    public MobileElement tools;
+
+    @AndroidFindBy(uiAutomator = "new UiScrollable(new UiSelector()).scrollIntoView(resourceIdMatches(\"flar2.devcheck:id/model\"))")
+    public MobileElement model;
+}
--- src/test/java/devcheck/Test_Dashboard.java
+++ src/test/java/devcheck/Test_Dashboard.java
@@ -2,19 +2,34 @@ package devcheck;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import pageObject.DashboardPage;
 
 import java.io.IOException;
 
 public class Test_Dashboard extends Base{
 
+    DashboardPage dashboard;
+
     @BeforeTest
     public void setUp() throws IOException {
         AppiumDriver<MobileElement> driver=start();
+        dashboard=new DashboardPage(driver);
     }
 
     @Test
-    void test(){
+    void verifySubMenus(){
+        Assert.assertTrue(dashboard.battery.isDisplayed(),"Battery not present");
+        Assert.assertTrue(dashboard.cpuStats.isDisplayed(),"CPU Status not present");
+        Assert.assertTrue(dashboard.monitors.isDisplayed(),"Monitors not present");
+        Assert.assertTrue(dashboard.benchmarks.isDisplayed(),"Benchmarks not present");
+        Assert.assertTrue(dashboard.network.isDisplayed(),"Network not present");
+        Assert.assertTrue(dashboard.ram.isDisplayed(),"RAM not present");
+        Assert.assertTrue(dashboard.temperatures.isDisplayed(),"Temperatures not present");
+        Assert.assertTrue(dashboard.tools.isDisplayed(),"Tools not present");
+        Assert.assertTrue(dashboard.model.isDisplayed(),"Model not present");
     }
 }
